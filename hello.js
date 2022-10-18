4e51794df2073abe2632bd952c44b8d279a936e5
Tenzing Sherpa <sherpatenzee09@gmail.com>
2022-10-18 09:17:20 -0400

    some js

---
+++ hello.js
@@ -0,0 +1,20 @@
+// Hello world
+
+"use strict";
+ var message="hello world";
+ console.log(message);
+
+
+ var num1 =56;
+ var num2 =20;
+ var answer1= num1 + num2;
+ var answer2 = num1-num2;
+ var answer3= num1* num2;
+ var answer4 = num1/ num2;
+ var answer5 = num1 % num2;
+
+ console.log("1:" + answer1);
+ console.log("2:" + answer2);
+ console.log("3:" + answer3);
+ console.log("4:" + answer4);
+ console.log("5:" + answer5);
