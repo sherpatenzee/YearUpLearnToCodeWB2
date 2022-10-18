9639640c216161ae90ff56a4b65d6148d7ac990e
Tenzing Sherpa <sherpatenzee09@gmail.com>
2022-10-18 11:13:12 -0400

    complete exercise

--- mathScripts ./rule_of_72.js
+++ mathScripts ./rule_of_72.js
@@ -1,6 +1,6 @@
-var saving = 50000;
+var saving = 1000000;
 var interest = 10;
 var double = saving * 2
-var years = 72/ interest;
+var years = 72 / interest;
 
-console.log("At a 10% interest rate, your savings account will be worth "+ double + " in " + years + " years");
+console.log("At a 10% interest rate, your savings account will be worth "+ double.toFixed(2) + " in " + years.toFixed(1) + " years");
