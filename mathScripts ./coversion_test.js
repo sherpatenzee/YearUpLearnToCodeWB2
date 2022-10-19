f2037102b76b9daa0a078ef1c4e27a88a19291ea
Tenzing Sherpa <sherpatenzee09@gmail.com>
2022-10-19 09:30:19 -0400

    complete exercise

---
+++ mathScripts ./coversion_test.js
@@ -0,0 +1,65 @@
+var a = " 101.1 "; 
+var b = "55";
+var c = "402 Stevens"; 
+var d = "Number 5 ";
+
+//pasrseInt
+var a = parseInt(a);
+console.log(a)
+var b = parseInt(b);
+console.log(b)
+var c = parseInt(c);
+console.log(c)
+var d = parseInt(d);
+console.log(d)
+
+
+var a = " 101.1 "; 
+var b = "55";
+var c = "402 Stevens"; 
+var d = "Number 5 ";
+//parseFloat
+
+var a = parseFloat(a);
+console.log(a)
+var b = parseFloat(b);
+console.log(b)
+var c = parseFloat(c);
+console.log(c)
+var d = parseFloat(d);
+console.log(d)
+
+var a = " 101.1 "; 
+var b = "55";
+var c = "402 Stevens"; 
+var d = "Number 5 ";
+
+//number()
+
+var a = Number(a);
+console.log(a)
+var b = Number(b);
+console.log(b)
+var c = Number(c);
+console.log(c)
+var d = Number(d);
+console.log(d)
+
+//unary+
+
+var a = " 101.1 "; 
+var b = "55";
+var c = "402 Stevens"; 
+var d = "Number 5 ";
+
+a = +a;
+console.log(a)
+
+b = +b;
+console.log(b)
+
+c = +c;
+console.log(c)
+
+d = +d;
+console.log(d)
